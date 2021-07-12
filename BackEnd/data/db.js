c4f56fa192f1ee4aac002158d22ebddbe406fa7d
Joao4Rodrigues <86954685+Joao4Rodrigues@users.noreply.github.com>
2021-07-12 16:49:28 +0100

    backEnd start
    backEnd start

--- BackEnd/data/db.js
+++ BackEnd/data/db.js
@@ -1,31 +1,88 @@
 const mongodb = require('mongodb');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const { MongoClient } = mongodb;
 
 const URI = "mongodb://localhost:27017"
-const DB_NAME = "auth-demo"
-let client
+const DB_NAME = "Ser_Aluno"
+let aluno  
 
 async function connect(uri) {
     try {
 
-        if (client) return client
+        if (aluno) return aluno
 
-        client = new MongoClient(uri, {
+        aluno = new MongoClient(uri, {
             useUnifiedTopology: true
         })
 
-        await client.connect();
+        await aluno.connect();
 
-        return client
+        return aluno
     } catch (err) {
         console.log(err)
     }
 }
 
 async function getCollection(dbName, collectionName) {
-    const client = await connect(URI);
-    const db = client.db(dbName);
+    const aluno = await connect(URI);
+    const db = aluno.db(dbName);
     return db.collection(collectionName);
+}
+
+async function insereAluno(aluno) {
+    const collection = await getCollection(DB_NAME, "alunos");
+    aluno.password = await bcrypt.hash(aluno.password, saltRounds);
+    const res = await collection.insertOne(user)
+    return res.insertedId;
+}
+
+async function obtemAluno(id) {
+    const collection = await getCollection(DB_NAME, "alunos");
+    const res = await collection.findOne({_id: mongodb.ObjectId(id)})
+    return res;
+}
+
+async function obtemAnoLetivo(anoLetivo) {
+    const collection = await getCollection(DB_NAME, "alunos");
+    const res = await collection.findOne({_anoLetivo: mongodb.ObjectId(anoLetivo)})
+    return res;
+}
+
+async function obtemLivros(livros) {
+    const collection = await getCollection(DB_NAME, "alunos");
+    const res = await collection.findOne({_livros: mongodb.ObjectId(livros)})
+    return res;
+}
+
+
+async function insereSessao(uid) {
+    const collection = await getCollection(DB_NAME, "sessoens");
+    const res = await collection.insertOne({
+        uid,
+        expiresAt: new Date(new Date().valueOf() + (5 * 60 * 1000))
+    })
+    return res.insertedId; 
+}
+
+async function obtemSessao(id) {
+    const collection = await getCollection(DB_NAME, "sessoens");
+    const res = await collection.findOne({_id: mongodb.ObjectId(id)})
+    return res; 
+}
+
+async function sessaoPrelongada(id) {
+    const collection = await getCollection(DB_NAME, "sessoens");
+    const res = await collection.updateOne(
+        {_id: mongodb.ObjectId(id)},
+        {
+            $set: {
+                expiresAt: new Date(new Date().valueOf() + (5 * 60 * 1000))
+            }
+        }
+    )
+    return res; 
 }
 
 // async function insertUser(user) {
@@ -36,5 +93,10 @@ async function getCollection(dbName, collectionName) {
 // }
 
 module.exports = { 
-    
+    insereAluno,
+    obtemAluno,
+    obtemAnoLetivo,
+    insereSessao,
+    obtemSessao,
+    sessaoPrelongada    
 }
--- BackEnd/data/express.js
+++ BackEnd/data/express.js
@@ -1,5 +1,15 @@
 const express = require('express');
-// const {  } = require('./db');
+ const { 
+    insereAluno,
+    obtemAluno,
+    obtemAnoLetivo,
+    obtemLivros,
+    insereSessao,
+    obtemSessao,
+    sessaoPrelongada  
+} = require('./db');
+
+const bcrypt = require('bcrypt');
 
 const PORT = 3001;
 const server = express();
@@ -12,10 +22,64 @@ server.get('/', (req, res) => {
     })
 })
 
+server.post("/auth", async (req, res) => {
+    const { username, password } = req.body;
+    const aluno = await findUser(username);
+    if (aluno && await bcrypt.compare(password, aluno.password)) {
+        const sessionId = await insertSession(aluno._id)
+        res.status(200).json({ token: sessionId })
+    } else {
+        res.sendStatus(404)
+    }
+})
+
+async function verificaAluno(req, res, next) {
+    console.log(req.headers)
+    const auth = req.headers.authorization?.split(' ') ?? []
+    console.log(auth)
+    if (auth.length > 0) {
+        const session = await findSession(auth[1])
+        if (session && session.expiresAt > new Date()) {
+            await extendSession(session._id)
+            req.user = await findUserById(session.uid);
+            next()
+        } else {
+            res.sendStatus(401)
+        }
+    } else {
+        res.sendStatus(401)
+    }
+}
+
+server.get("/aluno", verificaAluno, async (req, res) => {
+    res.status(200).json({ aluno: req.aluno.nome })
+})
+
+server.post("/aluno", async (req, res) => {
+    const id = await insereAluno(req.body)
+    res.status(200).json({ id })
+})
+
 server.get('/obtemAnoLetivo', (req, res) => {
     anoLetivo = '5º Ano'
     res.status(200).json(anoLetivo)
 })
+
+server.post("/obtemLivros", async (req, res) => {
+    const livros = await obtemLivros(req.body)
+    
+    if (anoLetivo) {
+        const anoLetivo = await obtemAnoLetivo(anoLetivo._anoLetivo)
+    } else {
+        res.sendStatus(401)
+    }
+    res.status(200).json({ livros })
+})
+
+// Tentar perceber como fazer
+/* server.get('/obtemLivros', (req, res) => {
+    res.status(200).json({ anoLetivo: req.anoLetivo.livros })
+}) */
 
 
 
