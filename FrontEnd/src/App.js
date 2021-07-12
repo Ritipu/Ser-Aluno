24c15f0bc87ed3a6c13694c4fbfcfb9c24d90170
Ritipu <82657453+Ritipu@users.noreply.github.com>
2021-07-12 17:09:51 +0100

    chore:navbar & footer
    chore:navbar & footer

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -22,19 +22,35 @@ export default class App extends React.Component {
 	render() {
 		if (this.state.isLoggedIn === false) {
 			return (
-				<div className="navbar">
-					<img src="/logo.png" height="30px" alt="Ser Aluno"/>
-					<button onClick={this.acessPlatform}>Login</button>
-					<button onClick={this.acessPlatform}>
-					<img src="https://www.autenticacao.gov.pt/o/autenticacao-gov-theme/images/logo/logo-ag.svg"
-						alt="Logo Autenticação Gov" />
-					</button>
+				<div>
+					<div className="navbar">
+						<img className="navbar-logo" src="/logo.png" height="40px" alt="Ser Aluno" />
+						<div className="navbar-btn">
+							<button className="auth-login" onClick={this.acessPlatform}>Login</button>
+							<button className="auth-gov" onClick={this.acessPlatform}>
+								<img src="https://www.autenticacao.gov.pt/o/autenticacao-gov-theme/images/logo/logo-ag.svg"
+									alt="Logo Autenticação Gov" />
+							</button>
+						</div>
+
+					</div>
+					<div className="body">
+						Carousel
+					</div>
+					<div className="footer">
+						<img src="/logos/portoEditora.png" height="60px" />
+						<img src="/logos/agaKhan.png" height="60px" />
+						<img src="/logos/Logo_Portugal_2020.png" height="60px" />
+						<img src="/logos/logo_segurancaSocial.png" height="60px" />
+						<img src="/logos/republica.png" height="60px" />
+						<img src="/logos/uniaoEuropeia.png" height="60px" />
+					</div>
 				</div>
 			);
 		} else {
 			return (
 				<div>
-					<Plataforma logout={() => this.logout()}/>
+					<Plataforma logout={() => this.logout()} />
 				</div>
 			)
 		}
